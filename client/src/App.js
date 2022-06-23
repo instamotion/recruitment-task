0985fe834a91dbf1b03e07dd3c5c34a65f864f9f
Daniyal Hamid <d.hamid@instamotion.com>
2022-06-23 13:59:27 +0200

    Initial commit frontend

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+    const [orders, setOrders] = useState([]);
+
+    const backendUrl = "http://localhost:3001";
+
+    const client = axios.create({
+        baseURL: backendUrl,
+    });
+
+    const loadOrders = useCallback(async () => {
+        const orders = await client.get('/orders');
+        setOrders(orders.data);
+    }, [client]);
+
+    const handleAddOrder = async () => {
+        await client.post('/orders');
+        await loadOrders();
+    };
+
+    const handleDeleteOrder = async (id) => {
+        await client.delete(`/orders/${id}`);
+        await loadOrders();
+    };
+
+    useEffect(() => {
+        (async () => {
+            await loadOrders();
+        })();
+    }, [setOrders, loadOrders]);
+
+    return (
+        <div className="App">
+            <button onClick={handleAddOrder}>Add Order</button>
+            {orders.map((order) => (
+                <div key={order._id}>
+                    Order { order._id }
+                    <button onClick={() => handleDeleteOrder(order._id)}>Complete</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default App;
